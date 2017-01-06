b9be5b265acdec826f396d7727b6a257136cc716
Ian G <yo.ian.g@gmail.com>
2017-01-05 21:00:44 -0500

    adding vendor and godeps files

---
+++ vendor/github.com/steadyequipment/goutility/network.go
@@ -0,0 +1,61 @@
+package goutility
+
+import (
+	"errors"
+	"net"
+)
+
+// GetFirstNonLoopbackIP get first non-loopback ip address of the current machine
+func GetFirstNonLoopbackIP() (string, error) {
+
+	// https://gist.github.com/mowings/017c80c188d1024ba3e7
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range addrs {
+
+		ip := GetAvailableIPFromAddress(addr)
+
+		// TODO: clarify function name, we're only looking for IPv4s here
+		// TODO: clarify function name, we're returning a string, not a net.IP
+		ipString := retreiveNonLoopbackV4IPString(ip)
+		if ipString != nil {
+			return *ipString, nil
+		}
+	}
+	return "", errors.New("Unable to find external ip address")
+}
+
+func retreiveNonLoopbackV4IPString(ip net.IP) *string {
+
+	if ip == nil {
+		return nil
+	}
+
+	if ip.IsLoopback() {
+		return nil
+	}
+
+	ipv4 := ip.To4()
+	if ipv4 == nil {
+		return nil
+	}
+
+	ipString := ipv4.String()
+	return &ipString
+}
+
+// GetAvailableIPFromAddress retrieves the IP from a net.Addr if one is available
+func GetAvailableIPFromAddress(address net.Addr) net.IP {
+
+	var ip net.IP
+	switch v := address.(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	return ip
+}
